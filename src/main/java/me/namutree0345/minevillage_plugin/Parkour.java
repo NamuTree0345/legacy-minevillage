fda41667c11f7cbef5167406bba9a72a313779ad
TreeKR <johanbag290@gmail.com>
2021-03-06 22:54:15 +0900

    test

--- src/main/java/me/namutree0345/minevillage_plugin/Parkour.java
+++ src/main/java/me/namutree0345/minevillage_plugin/Parkour.java
@@ -16,6 +16,10 @@ public class Parkour implements Listener {
 
     public HashMap<UUID, Location> recentCheckpoint = new HashMap<>();
 
+    private void printLoc(Location loc) {
+        System.out.printf("(%f, %f, %f), (%d, %d, %d)\n", loc.getX(), loc.getY(), loc.getZ(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+    }
+
     @EventHandler
     public void move(PlayerMoveEvent event) {
         /*
@@ -30,12 +34,16 @@ public class Parkour implements Listener {
             }
         }
         if(loc2.getType() == Material.GOLD_BLOCK) {
+            System.out.print("A: ");
+            printLoc(loc.toBlockLocation());
+            System.out.print("B: ");
+            printLoc(recentCheckpoint.get(event.getPlayer().getUniqueId()));
             if(!recentCheckpoint.containsKey(event.getPlayer().getUniqueId())) {
-                recentCheckpoint.put(event.getPlayer().getUniqueId(), event.getTo().toBlockLocation());
+                recentCheckpoint.put(event.getPlayer().getUniqueId(), loc.toBlockLocation());
                 event.getPlayer().sendTitle(ChatColor.GREEN + "시작!", "", 10, 70, 10);
             } else {
-                if(recentCheckpoint.get(event.getPlayer().getUniqueId()) != event.getTo().toBlockLocation()) {
-                    recentCheckpoint.replace(event.getPlayer().getUniqueId(), event.getTo().toBlockLocation());
+                if(recentCheckpoint.get(event.getPlayer().getUniqueId()) != loc.toBlockLocation()) {
+                    recentCheckpoint.replace(event.getPlayer().getUniqueId(), loc.toBlockLocation());
                     event.getPlayer().sendTitle(ChatColor.GOLD + "체크포인트 달성!", "", 10, 70, 10);
                     event.getPlayer().sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + event.getPlayer().getName() + ChatColor.RESET + "" + ChatColor.GOLD + "님이 어떤 체크포인트를 찍었습니다!");
                     event.getPlayer().getWorld().spawnParticle(Particle.FIREWORKS_SPARK, loc.getX(), loc.getY() - 1, loc.getZ(), 100, 3, 3, 3);
