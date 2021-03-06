3fd9bd905b50d5c65f046d0e6d05ca4a3a5fa8c9
TreeKR <johanbag290@gmail.com>
2021-03-06 22:13:13 +0900

    test

--- src/main/java/me/namutree0345/minevillage_plugin/Parkour.java
+++ src/main/java/me/namutree0345/minevillage_plugin/Parkour.java
@@ -18,9 +18,12 @@ public class Parkour implements Listener {
 
     @EventHandler
     public void move(PlayerMoveEvent event) {
+        /*
         Location loc = event.getTo();
         loc.setY(loc.getY() - 1);
-        Block loc2 = event.getPlayer().getWorld().getBlockAt(loc);
+         */
+        Location loc = event.getTo();
+        Block loc2 = event.getPlayer().getWorld().getBlockAt(event.getPlayer().getLocation().getBlockX(), event.getPlayer().getLocation().getBlockY() - 1, event.getPlayer().getLocation().getBlockZ());
         if(recentCheckpoint.containsKey(event.getPlayer().getUniqueId())) {
             if(loc2.getType() == Material.RED_WOOL) {
                 event.getPlayer().teleport(recentCheckpoint.get(event.getPlayer().getUniqueId()));
@@ -31,7 +34,7 @@ public class Parkour implements Listener {
                 recentCheckpoint.put(event.getPlayer().getUniqueId(), event.getTo());
                 event.getPlayer().sendTitle(ChatColor.GOLD + "체크포인트 달성!", "", 10, 70, 10);
                 event.getPlayer().sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + event.getPlayer().getName() + ChatColor.RESET + "" + ChatColor.GOLD + "님이 어떤 체크포인트를 찍었습니다!");
-                event.getPlayer().getWorld().spawnParticle(Particle.FIREWORKS_SPARK, loc.getX(), loc.getY(), loc.getZ(), 100, 3, 3, 3);
+                event.getPlayer().getWorld().spawnParticle(Particle.FIREWORKS_SPARK, loc.getX(), loc.getY() - 1, loc.getZ(), 100, 3, 3, 3);
             }
         }
     }
